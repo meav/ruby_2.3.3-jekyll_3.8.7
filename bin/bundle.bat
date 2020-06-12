a29bc608564334c2f3a5cd363d7d93cc653c3dc0
meav <milanistavietnam@gmail.com>
2020-06-12 13:58:23 +0700

    First

---
+++ bin/bundle.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"A:\bin\ruby.exe" "C:/Users/vhd/Desktop/meav.github.io-master/ruby/bin/bundle" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"A:\bin\ruby.exe" "%~dpn0" %*
