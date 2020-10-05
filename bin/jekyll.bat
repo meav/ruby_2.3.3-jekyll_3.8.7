31fce2ae333e5fc684090fa48998c4465973e28a
meav <milanistavietnam@gmail.com>
2020-10-05 22:26:01 +0700

    Cygwin

--- bin/jekyll.bat
+++ bin/jekyll.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"A:\bin\ruby.exe" "A:/bin/jekyll" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"C:\Users\vhd\Music\cygwin\cygwin\home\meav\.bin\ruby_2.3.3-jekyll_3.8.7\bin\ruby.exe" "C:/Users/vhd/Music/cygwin/cygwin/home/meav/.bin/ruby_2.3.3-jekyll_3.8.7/bin/jekyll" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"A:\bin\ruby.exe" "%~dpn0" %*
+@"C:\Users\vhd\Music\cygwin\cygwin\home\meav\.bin\ruby_2.3.3-jekyll_3.8.7\bin\ruby.exe" "%~dpn0" %*
